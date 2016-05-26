dace8f3166cb1d88796db485ad31776810a8df03
yaron <yaronv99@gmail.com>
2016-05-26 16:25:11 +0300

    uncomment

--- frontend/app/Global.scala
+++ frontend/app/Global.scala
@@ -6,7 +6,7 @@ import play.api.Play.current
 object Global extends GlobalSettings {
   override def onStart(app: Application) = {
     // use sbt start to launch the app: https://github.com/playframework/playframework/issues/2212
-//    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
 
     ClassificationService.initialize()
   }
